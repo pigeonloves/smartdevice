92b3c41483bd58e99dec5c8460e7ee41bbd792eb
pigeonloves <and7.milltir@gmail.com>
2022-02-22 11:27:44 +0300

    Добавляет anchor.js

---
+++ source/js/anchor.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Прокрутка до якоря
+
+(function () {
+  var links = document.querySelectorAll('[data-link]');
+
+  if (links) {
+    for (var i = 0; i < links.length; i++) {
+
+      links[i].addEventListener('click', function (evt) {
+
+        var target = document.querySelector('[id=' + evt.target.dataset.link + ']');
+
+        if (target) {
+          target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+          });
+        }
+      });
+    }
+  }
+
+})();
