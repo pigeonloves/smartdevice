eaa927b82edfac84eaf4f3c25dbed18c70a28c5a
pigeonloves <and7.milltir@gmail.com>
2022-02-22 11:28:50 +0300

    Добавляет validate.js

---
+++ source/js/validate.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Валидация номера телефона
+
+(function () {
+
+  var phoneNumber = document.querySelectorAll('[type=tel]');
+
+  var setMask = function (input) {
+    input.addEventListener('focus', function (evt) {
+      if (!input.value && evt.key !== window.utils.KeyCode.BACKSPACE) {
+        input.value = '+7(';
+      } else {
+        return;
+      }
+    });
+
+    input.addEventListener('keydown', function (evt) {
+      var numberLength = input.value.length;
+
+      if (numberLength === 6 && evt.key !== window.utils.KeyCode.BACKSPACE) {
+        input.value = input.value + ')';
+      }
+    });
+  };
+
+  if (phoneNumber) {
+    for (var i = 0; i < phoneNumber.length; i++) {
+      setMask(phoneNumber[i]);
+    }
+  }
+
+})();
