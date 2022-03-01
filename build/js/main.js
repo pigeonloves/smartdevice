232c7e5ddccace6a54526b3be2e7e610f220590b
pigeonloves <and7.milltir@gmail.com>
2022-03-01 08:38:37 +0300

    Вносит правки

--- build/js/main.js
+++ build/js/main.js
@@ -8,6 +8,26 @@
         }
     };
 }());
+// Меняет текст в разделе "О компании"
+(function () {
+    var WORDS_COUNT = 21;
+    var DOTS = '..';
+    var tablet = window.matchMedia('(max-width: 1023px)');
+    var textContainer = document.querySelector('.about p:last-of-type');
+    var desktopText = textContainer.innerText;
+    var shortText = desktopText.split(' ', WORDS_COUNT).join(' ').concat(DOTS);
+    if (textContainer) {
+        var changeTextHandler = function (evt) {
+            if (evt.matches) {
+                textContainer.innerText = shortText;
+            } else {
+                textContainer.innerText = desktopText;
+            }
+        };
+    }
+    tablet.addListener(changeTextHandler);
+    changeTextHandler(tablet);
+}());
 // Аккордион (мобильная версия футера)
 (function () {
     var navList = document.querySelector('.footer__list-wrap');
